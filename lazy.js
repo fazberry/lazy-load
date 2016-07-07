44d3ad894968dedc3671727368ec69be7e6bde6f
fazberry <rxfarry@googlemail.com>
2016-07-07 19:33:59 +0100

    Initial

---
+++ lazy.js
@@ -0,0 +1,33 @@
+$(function() {
+    function lazyLoad(x) {
+        x += $(window).height() + 100;
+        $('[data-background],[data-image]').each(function(){
+            var self = $(this);
+            if(self.offset().top > x) {
+                return;
+            }
+            if(self.data('background')){
+                var image = self.data('background');
+            } else {
+                var image = self.data('image');
+            }
+            $('<img>',{src:image}).on('load', function(){
+                if(self.data('background')){
+                    self.css('background-image', 'url("'+image+'")');
+                } else {
+                    self.attr('src', image);
+                }
+                self.removeAttr('data-background');
+                self.removeAttr('data-image');
+            })
+        });
+    }
+
+    //&& !self.closest('.grid').length
+
+    $(window).scroll(function(){
+        var x = $(this).scrollTop();
+        lazyLoad(x);
+    });
+    lazyLoad(0);
+});
